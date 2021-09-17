66af1dd6ea48b224520072f888fcbb6237910754
Freewind <nowindlee@gmail.com>
2021-09-17 15:06:40 +0800

    fix vertical middle

--- src/ItemWithIcons/ItemWithIcons.tsx
+++ src/ItemWithIcons/ItemWithIcons.tsx
@@ -6,9 +6,9 @@ type Props = {
 }
 
 export const ItemWithIcons: FC<PropsWithChildren<Props>> = ({start, children, end}) => {
-  return <div className='d-flex p-1'>
-    {start && <span className='flex-grow-0'>{start}</span>}
-    <span className='flex-grow-1 ms-1 me-1'>{children}</span>
-    {end && <span className='flex-grow-0'>{end}</span>}
+  return <div className='d-flex p-1 align-items-center' style={{border: '1px solid red'}}>
+    {start && <span className='flex-grow-0 align-middle'>{start}</span>}
+    <span className='flex-grow-1 ms-1 me-1 align-middle' style={{fontSize: 40}}>{children}</span>
+    {end && <span className='flex-grow-0 align-middle'>{end}</span>}
   </div>
 }
--- src/ListItemWithIcons.tsx
+++ src/ListItemWithIcons.tsx
@@ -5,8 +5,8 @@ import {ArrowLeft, ArrowRight} from 'react-bootstrap-icons';
 
 
 export const ListItemWithIcons: FC = () => {
-  return <ListGroup style={{width: 200}}>
-    <ItemWithIcons start={<ArrowLeft/>} end={<ArrowRight/>}>Hello1</ItemWithIcons>
+  return <ListGroup style={{width: 300}}>
+    <ItemWithIcons start={<ArrowLeft />} end={<ArrowRight/>}>Hello1</ItemWithIcons>
     <ItemWithIcons start={<ArrowLeft/>} end={<ArrowRight/>}>Hello2</ItemWithIcons>
     <ItemWithIcons start={<ArrowLeft/>} end={<ArrowRight/>}>Hello3</ItemWithIcons>
   </ListGroup>;
